637740155828030dfbbe73c6055f689555305f0d
Jionerd <emmaaustin509@gmail.com>
2023-06-15 19:17:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - output: phrase with proper grammar, 
+ * main - output: phrase with proper grammar,
  * but the outcome is a piece of art, (and a new line)
  * Return: 0 if exited properly, non-zero otherwise
  */
