2b213a915083f3f01333fccc5ced48c0f2bc9da8
Jionerd <emmaaustin509@gmail.com>
2023-06-15 19:16:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - output: phrase with proper grammar, but the outcome is a piece of art, (and a new line)
+ * main - output: phrase with proper grammar, 
+ * but the outcome is a piece of art, (and a new line)
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
